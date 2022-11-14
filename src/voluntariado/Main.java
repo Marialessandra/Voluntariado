c9b7c6cb90c407b0f4b3e33bbdfb81ff79709322
joreims <jorge.garcia14624@est.uca.edu.ni>
2022-11-13 21:57:11 -0600

    Añado primera version sin login

---
+++ src/voluntariado/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package voluntariado;
+
+import formularios.Login;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author jegar
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame Login = new Login();
+        Login.setVisible(true);
+    }
+    
+}
